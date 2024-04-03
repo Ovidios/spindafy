0f845ecb56d5616f78b3507f46d118bdc00d3d55
Jakob Harych <jakob@harych.de>
2024-04-03 21:31:46 +0200

    fix for transparent target images

--- spinda_optimizer.py
+++ spinda_optimizer.py
@@ -56,11 +56,12 @@ def evolve_step(target, population):
 
 def evolve(target, pop, n_generations, include = []):
     # check for predefined spinda patterns!
+    black_target = [127, 127, 127, 255] if target.mode == "RBGA" else 127
     if np.all(np.greater_equal(target, 128)):
         best_spinda = SpindaConfig.from_personality(PREDEFINED["ALL_WHITE"])
         print(f"Found predefined Spinda: 'ALL_WHITE': {hex(best_spinda.get_personality())}")
         return (best_spinda.get_difference(target), best_spinda)
-    if np.all(np.less_equal(target, [127, 127, 255])):
+    if np.all(np.less_equal(target, black_target)):
         best_spinda = SpindaConfig.from_personality(PREDEFINED["ALL_BLACK"])
         print(f"Found predefined Spinda: 'ALL_BLACK': {hex(best_spinda.get_personality())}")
         return (best_spinda.get_difference(target), best_spinda)
