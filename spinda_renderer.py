b46718a29d1d7af509245a9fb2dabf62e86e18d1
Jakob Harych <jakob@harych.de>
2024-04-17 22:36:56 +0200

    add command line utility for generating spindas based on pid

---
+++ spinda_renderer.py
@@ -0,0 +1,22 @@
+from spindafy import SpindaConfig
+from argparse import ArgumentParser
+from PIL import Image
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("pid")
+    parser.add_argument("--scale", dest="scale", default=1, action="store", nargs="?", type=int)
+    parser.add_argument("--save", action='store_true')
+    parser.add_argument("--show", action='store_true')
+
+    args = parser.parse_args()
+
+    config = SpindaConfig.from_personality(int(args.pid, 16))
+    img = config.render_pattern()
+
+    img = img.resize((img.width*args.scale, img.height*args.scale), Image.Resampling.NEAREST)
+
+    if args.save:
+        img.save(hex(config.get_personality()) + ".png")
+    if args.show or not args.save:
+        img.show()
